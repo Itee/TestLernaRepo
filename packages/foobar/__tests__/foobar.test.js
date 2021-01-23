fbd64e0a62bce8a560e43586c1e01b098cf5150b
Tristan VALCKE <valcketristan@gmail.com>
2021-01-23 16:26:02 +0100

    FooBar: Create new package foobar

---
+++ packages/foobar/__tests__/foobar.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const foobar = require('..');
+
+describe('foobar', () => {
+    it('needs tests');
+});
---
+++ packages/foobar/lib/foobar.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = foobar;
+
+function foobar() {
+    // TODO
+}
